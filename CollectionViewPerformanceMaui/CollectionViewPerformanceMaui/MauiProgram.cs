b608599291ae2943f0a3c59cdaf4d067d9d7b1fb
Ethan Ellis <eth.ellis@outlook.com>
2024-11-23 02:05:19 +1030

    Initial commit

---
+++ CollectionViewPerformanceMaui/CollectionViewPerformanceMaui/MauiProgram.cs
@@ -0,0 +1,62 @@
+﻿using CollectionViewPerformanceMaui.Services;
+using CollectionViewPerformanceMaui.ViewModels;
+using CollectionViewPerformanceMaui.Views;
+
+#if ANDROID
+using CollectionViewPerformanceMaui.Controls;
+using CollectionViewPerformanceMaui.Platforms.Android.Handlers;
+#endif
+
+namespace CollectionViewPerformanceMaui
+{
+    public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+					fonts.AddFont("FontAwesomeBrands.otf", "FA-B");
+                })
+				.ConfigureMauiHandlers(handlers =>
+				{
+#if ANDROID
+					handlers.AddHandler<Card, CardHandler>();
+
+					CardHandler.SetupMapper();
+#endif
+                })
+				.RegisterServices()
+				.RegisterViewModels()
+				.RegisterViews();
+
+			return builder.Build();
+		}
+
+		public static MauiAppBuilder RegisterServices(this MauiAppBuilder mauiAppBuilder)
+		{
+			mauiAppBuilder.Services.AddSingleton<IDataService, DataService>();
+
+			return mauiAppBuilder;
+		}
+
+		public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+		{
+			mauiAppBuilder.Services.AddSingleton<DataViewModel>();
+
+			return mauiAppBuilder;
+		}
+
+		public static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+		{
+			mauiAppBuilder.Services.AddSingleton<DataView>();
+
+			return mauiAppBuilder;
+		}
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Issue Repro
+
+MauiAppCollectionViewFooterResize
+
+[CollectionView Items display issue when Header is resized on iOS](https://github.com/dotnet/maui/issues/12429)
+
+---
+
+VirtualListEmptyViewOverlap
+
+[EmptyView overlaps with GlobalHeader, GlobalFooter and Content instead of going between GlobalHeader and GlobalFooter](https://github.com/Redth/Maui.VirtualListView/issues/31)
+
+---
+
+CollectionViewPerformanceMaui
+
+CollectionViewPerformanceXamarin
+
+[[iOS/Android] CollectionView scrolling performance worse in .NET MAUI when compared to Xamarin.Forms (lag/stutter)](https://github.com/dotnet/maui/issues/21580)
