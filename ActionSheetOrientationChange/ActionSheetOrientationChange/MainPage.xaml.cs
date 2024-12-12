fff5a51796f2be4330ea41682a6c83a9455a1a49
Ethan Ellis <eth.ellis@outlook.com>
2024-12-12 17:16:05 +1030

    Add ActionSheetOrientationChange

---
+++ ActionSheetOrientationChange/ActionSheetOrientationChange/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ActionSheetOrientationChange;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        DisplayActionSheet("ActionSheet", "Cancel", "Destruction", "Option1", "Option2", "Option3");
+    }
+}
